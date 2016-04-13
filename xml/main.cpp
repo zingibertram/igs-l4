a2959336da6f12b29f4e386c64a0551bde493497
Arseny Akhtyamov <zingibertram@gmail.com>
2016-04-13 21:03:17 +0500

    Пример стилей приложения

--- xml/main.cpp
+++ xml/main.cpp
@@ -1,10 +1,21 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QStyleFactory>
+#include <qdebug.h>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+//    a.setStyle(QStyleFactory::create("Fusion"));
+//    QPalette p = a.palette();
+//    p.setColor(QPalette::Window, QColor(53,53,53));
+//    p.setColor(QPalette::Button, QColor(53,53,53));
+//    p.setColor(QPalette::Highlight, QColor(142,45,197));
+//    p.setColor(QPalette::ButtonText, QColor(255,255,255));
+//    p.setColor(QPalette::WindowText, QColor(255,255,255));
+//    a.setPalette(p);
+//    qDebug() << QStyleFactory::keys();
     MainWindow w;
     w.show();
 
